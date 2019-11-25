810a1af532f1efa3c35444f2452909b1e94ca5ec
Jimzy Lui <Jimzy.Lui@gmail.com>
2019-11-24 23:16:07 -1000

    Updates

---
+++ precache-manifest.8bda11d261d1e6bc02347e583115158f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd7ca5172a7a365e3af52c63f34b7e48",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d783fd3f0a1c8717a44a",
+    "url": "/monsters-rolodex/static/css/main.96d68136.chunk.css"
+  },
+  {
+    "revision": "8256d7d0304953484fc5",
+    "url": "/monsters-rolodex/static/js/2.f5605220.chunk.js"
+  },
+  {
+    "revision": "d783fd3f0a1c8717a44a",
+    "url": "/monsters-rolodex/static/js/main.007a7b61.chunk.js"
+  },
+  {
+    "revision": "1b1f57fd6c4b40aa49d9",
+    "url": "/monsters-rolodex/static/js/runtime-main.10abe5f5.js"
+  }
+]);
